90529b6bb1b3d4258c4d4d34622aff1575c6f5a5
gustavo brito <gustavo.brito@open-co.br>
2023-07-02 17:41:28 -0300

    tkinter interface

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-from managers import FileManager, ScriptManager
-from pathlib import Path
+import tkinter as tk
+from tkinter import filedialog
+import os
 
-file_path = Path() / 'script_test.txt'
-file = FileManager(file_path.absolute())
+selected_script_path = ""
 
-while True:
-    command_and_param = file.next_line()
-    if command_and_param:
-        ScriptManager(*command_and_param).execute()
+
+def select_file():
+    global selected_script_path
+    selected_script_path = filedialog.askopenfilename()
+    update_run_button_state()
+    if selected_script_path:
+        file_button.config(text=os.path.basename(selected_script_path))
+
+
+def run_program():
+    global selected_script_path
+
+
+def update_run_button_state():
+    if selected_script_path:
+        run_button.config(state=tk.NORMAL)
     else:
-        break
+        run_button.config(state=tk.DISABLED)
+
+
+window = tk.Tk()
+window.title("Input Flow")
+window.geometry("473x627")
+
+button_frame = tk.Frame(window)
+button_frame.pack(pady=10)
+
+file_button = tk.Button(button_frame, text="Selecionar...", command=select_file)
+file_button.pack(side=tk.LEFT, padx=5)
+
+run_button = tk.Button(button_frame, text="Executar", command=run_program, state=tk.DISABLED)
+run_button.pack(side=tk.LEFT, padx=5)
+
+window.mainloop()
